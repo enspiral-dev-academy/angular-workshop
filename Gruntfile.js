77d12ad65dbe5c74c09e8eeafe180f842cf99e7e
Joshua Vial <joshua@enspiral.com>
2014-04-23 15:07:14 -0700

    adding grunt protractor runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -371,6 +371,23 @@ module.exports = function (grunt) {
         configFile: 'karma.conf.js',
         singleRun: true
       }
+    },
+    protractor: {
+      options: {
+        configFile: "node_modules/protractor/referenceConf.js", // Default config file
+        keepAlive: true, // If false, the grunt process stops when the test fails.
+        noColor: false, // If true, protractor will not use colors in its output.
+        args: {
+          // Arguments passed to the command
+        }
+      },
+      e2e: {
+        options: {
+          configFile: "./protractor.conf.js",
+          keepAlive: true,
+          args: {}
+        }
+      }
     }
   });
 
@@ -400,7 +417,8 @@ module.exports = function (grunt) {
     'concurrent:test',
     'autoprefixer',
     'connect:test',
-    'karma'
+    'karma',
+    'protractor:e2e'
   ]);
 
   grunt.registerTask('build', [
