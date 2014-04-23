fba5368f7348d1e6d7ecdd0876980cc11468b418
Joshua Vial <joshua@enspiral.com>
2014-04-23 15:55:40 -0700

    protractor specs for filter

--- spec/basics_spec.js
+++ spec/basics_spec.js
@@ -1,14 +1,24 @@
 'use strict';
-
 describe('basics', function() {
   beforeEach(function() {
     browser.get('/#/basics')
   });
 
-  it('updates searchText model', function() {
-    var input = element(by.model('searchText'));
-    input.sendKeys('my text');
-    expect(element(by.css('.searching')).getText()).toBe('my model is my text');
-  });
+  it('lists 3 cohorts', function() {
+    expect(element.all(by.css('.cohort-list tr')).count()).toBe(3);
+  })
 
+  describe('searching for cohorts', function() {
+    beforeEach(function() {
+      element(by.model('searchText')).sendKeys('slugs');
+    });
+
+    it('updates searchText model', function() {
+      expect(element(by.css('.searching')).getText()).toBe('my model is slugs');
+    });
+
+    it('filters cohorts by searchText', function() {
+      expect(element.all(by.css('.cohort-list tr')).count()).toBe(1)
+    });
+  });
 });
