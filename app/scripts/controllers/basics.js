2a642f13010b41cdda8fd8663e2e4d10379aea36
Joshua Vial <joshua@enspiral.com>
2014-04-23 13:57:37 -0700

    strip out controller guff

--- app/scripts/controllers/basics.js
+++ app/scripts/controllers/basics.js
@@ -2,9 +2,4 @@
 
 angular.module('demoApp')
   .controller('BasicsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
